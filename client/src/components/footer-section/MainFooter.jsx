437989ced0ba73c4e57ec8294551f15efa20a440
mmgeocode <mike.muncy85@gmail.com>
2024-09-19 11:06:32 -0600

    Adds icons and styles to footer

--- client/src/components/footer-section/MainFooter.jsx
+++ client/src/components/footer-section/MainFooter.jsx
@@ -1,17 +1,32 @@
 import emailIcon from '../assets/email-icon.png'
+import gitHubIcon from '../assets/github-icon.png'
+import linkedInIcon from '../assets/linkedin-logo.png'
+import credlyIcon from '../assets/credly-icon.png'
 
 function MainFooter(props) {
   return (
     <>
-        {/* <div style={{ background: "var(--nonary)", height: "5px"}}></div> */}
         <footer>
-          <div style={{ background: "var(--senary)", height: "5px"}}></div>
-          <div style={{ background: "var(--septenary)", height: "5px"}}></div>
-          <div style={{ background: "var(--octonary)", height: "5px"}}></div>
-          <h6 style={{ background: "var(--nonary)", height: "30px", padding: "0", marginBottom: "0"}}>2023 | Created by Mike Muncy</h6>
-
-          {/* <img src={emailIcon} alt="" /> */}
-
+          <div style={{ background: "var(--senary)", height: "5px", width: "100%" }}></div>
+          <div style={{ background: "var(--septenary)", height: "5px", width: "100%" }}></div>
+          <div style={{ background: "var(--octonary)", height: "5px", width: "100%" }}></div>
+          <div className="footerInfo" style={{ background: "var(--nonary", height: "100px", width: "100%", padding: "0", marginBottom: "0" }}>
+            <h6>2024 | Created by Mike Muncy</h6>
+            <ul className="socials">
+              <li>
+                <img src={emailIcon} alt="" />
+              </li>
+              <li>
+                <img src={gitHubIcon} alt="" />
+              </li>
+              <li>
+                <img src={linkedInIcon} alt="" />
+              </li>
+              <li>
+                <img src={credlyIcon} alt="" />
+              </li>
+            </ul>
+          </div>
         </footer>
     </>
   );
