9f725c1a1dd71d7190257c655e161789b0ab4b29
mmgeocode <mike.muncy85@gmail.com>
2024-09-20 11:05:12 -0600

    Fixes footer and adds styling

--- client/src/components/footer-section/MainFooter.jsx
+++ client/src/components/footer-section/MainFooter.jsx
@@ -10,20 +10,28 @@ function MainFooter(props) {
           <div style={{ background: "var(--senary)", height: "5px", width: "100%" }}></div>
           <div style={{ background: "var(--septenary)", height: "5px", width: "100%" }}></div>
           <div style={{ background: "var(--octonary)", height: "5px", width: "100%" }}></div>
-          <div className="footerInfo" style={{ background: "var(--nonary", height: "100px", width: "100%", padding: "0", marginBottom: "0" }}>
-            <h6>2024 | Created by Mike Muncy</h6>
+          <div className="footerInfo" style={{ background: "var(--nonary", height: "40px", width: "100%", padding: "0", marginBottom: "0" }}>
+                <h6>© 2024 by Mike Muncy</h6>
             <ul className="socials">
               <li>
-                <img src={emailIcon} alt="" />
+                <a href="mailto:mike.muncy85@gmail.com">
+                  <img src={emailIcon} alt="Email Mike" />
+                </a>
               </li>
               <li>
-                <img src={gitHubIcon} alt="" />
+                <a href="https://github.com/mmgeocode">
+                  <img src={gitHubIcon} alt="Mike's GitHub Profile" />
+                </a>
               </li>
               <li>
-                <img src={linkedInIcon} alt="" />
+                <a href="https://www.linkedin.com/in/mmuncy/">
+                  <img src={linkedInIcon} alt="Mike's LinkedIn Profile" />
+                </a>
               </li>
               <li>
-                <img src={credlyIcon} alt="" />
+                <a href="https://www.credly.com/users/mike-muncy">
+                  <img src={credlyIcon} alt="Mike's Credly Public Profile" />
+                </a>
               </li>
             </ul>
           </div>
